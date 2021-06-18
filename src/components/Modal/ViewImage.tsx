cfe79eb7a7903df86726febcfa6f379ba750441c
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-06-18 15:52:11 -0300

    Terminado modal de exibição de imagem do card

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -21,5 +21,25 @@ export function ModalViewImage({
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
   console.log(imgUrl);
-  return <h1>ta</h1>;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        maxWidth="900px"
+        maxHeight="650px"
+        bgColor="#1B1A18"
+        padding={0}
+        borderTopRadius={7}
+      >
+        <ModalBody padding={0}>
+          <Image src={imgUrl} borderTopRadius={7} />
+        </ModalBody>
+        <ModalFooter flex="1" height="100">
+          <Link href={imgUrl} isExternal>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
